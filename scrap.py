78fd7201ecebc24c4028ad59f51978f2cc11c490
Mo Azim <moazim1993@gmail.com>
2022-01-27 18:44:40 -0500

    Create scrap.py

---
+++ scrap.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Jan 27 18:25:46 2022
+
+@author: moazi
+"""
+
+def maxArea(height):
+    tab = [None]*(len(height)+1)
+    print(tab)
+    for i in height:
+        
+
+
+"""
+
+res1 = maxArea(height)
+print(res1)
+"""
+h2 = [2,3,4,5,18,17,6]
+
+res2 = maxArea(h2)
+print(res2)
