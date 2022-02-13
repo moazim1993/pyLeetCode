965bc4131ac5534847e08340349288d58a2596de
Mo Azim <moazim1993@gmail.com>
2022-02-13 16:35:40 -0500

    Create 3sum.py

---
+++ 3sum.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb 13 16:33:36 2022
+
+@author: moazi
+"""
+
+nums = [-1,0,1,2,-1,-4]
+
+def threeSum(nums):
+    
+    print(nums)
+
+
+print(threeSum(nums))
